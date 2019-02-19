058d33bf6f0516297f338469bc2380270a35983e
lucien <yuluqqcom@qq.com>
2019-02-19 17:34:02 +0800

    下拉组件

---
+++ DropdownView/pages/index/index.js
@@ -0,0 +1,127 @@
+//index.js
+//获取应用实例
+const app = getApp()
+var cityData = require('../../utils/city.js');
+
+Page({
+  data: {
+    cityleft: cityData.getCity(),
+    qyshow: true,
+    grade: '年级',
+    course: '教材',
+    section: '章节'
+  },
+  onLoad: function () {
+    
+  },
+  selectleft: function (e) {
+    this.setData({
+      cityright: {},
+      citycenter: this.data.cityleft[e.currentTarget.dataset.city],
+      select1: e.target.dataset.city,
+      select2: '',
+      grade: e.currentTarget.dataset.city
+    });
+  },
+  selectcenter: function (e) {
+    this.setData({
+      cityright: this.data.citycenter[e.currentTarget.dataset.city],
+      select2: e.target.dataset.city,
+      course: e.currentTarget.dataset.city
+    });
+  },
+  selectright: function (e) {
+    this.hidebg(e)
+    this.setData({
+      select3: e.target.dataset.city,
+      section: this.data.cityright[e.currentTarget.dataset.city] 
+    })
+  },
+  hidebg: function (e) {
+    this.setData({
+      qyopen: false,
+      nzopen: false,
+      pxopen: false,
+      nzshow: true,
+      pxshow: true,
+      qyshow: true,
+      isfull: false,
+      shownavindex: 0
+    })
+  },
+  listqy: function (e) {
+    if (this.data.qyopen) {
+      this.setData({
+        qyopen: false,
+        nzopen: false,
+        pxopen: false,
+        nzshow: true,
+        pxshow: true, // true
+        qyshow: true, // false
+        isfull: false,
+        shownavindex: 0
+      })
+    } else {
+      this.setData({
+        qyopen: true,
+        pxopen: false,
+        nzopen: false,
+        nzshow: true,
+        pxshow: true,
+        qyshow: false,
+        isfull: true,
+        shownavindex: e.currentTarget.dataset.nav
+      })
+    }
+  },
+  list: function (e) {
+    if (this.data.nzopen) {
+      this.setData({
+        nzopen: false,
+        pxopen: false,
+        qyopen: false,
+        nzshow: false,
+        pxshow: true,
+        qyshow: true,
+        isfull: false,
+        shownavindex: 0
+      })
+    } else {
+      this.setData({
+        nzopen: true,
+        pxopen: false,
+        qyopen: false,
+        nzshow: false,
+        pxshow: true,
+        qyshow: true,
+        isfull: true,
+        shownavindex: e.currentTarget.dataset.nav
+      })
+    }
+  },
+  listpx: function (e) {
+    if (this.data.pxopen) {
+      this.setData({
+        nzopen: false,
+        pxopen: false,
+        qyopen: false,
+        nzshow: true,
+        pxshow: false,
+        qyshow: true,
+        isfull: false,
+        shownavindex: 0
+      })
+    } else {
+      this.setData({
+        nzopen: false,
+        pxopen: true,
+        qyopen: false,
+        nzshow: true,
+        pxshow: false,
+        qyshow: true,
+        isfull: true,
+        shownavindex: e.currentTarget.dataset.nav
+      })
+    }
+  },
+})
